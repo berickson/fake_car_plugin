ace5dd2a655355cbdb99b27b6a46e03461ed851c
Brian Erickson <berickson@gmail.com>
2019-01-11 13:43:50 -0800

    Added wheel encoders

--- src/fake_car_plugin.cpp
+++ src/fake_car_plugin.cpp
@@ -5,6 +5,7 @@
 #include "ros/callback_queue.h"
 #include "ros/subscribe_options.h"
 #include "std_msgs/Float32.h"
+#include "std_msgs/Int32.h"
 
 namespace gazebo  {
     class FakeCarPlugin : public ModelPlugin 
@@ -12,15 +13,28 @@ namespace gazebo  {
         private: physics::ModelPtr model;
         private: physics::JointPtr fl_str_joint;
         private: physics::JointPtr fr_str_joint;
+        private: physics::JointPtr fl_axle_joint;
+        private: physics::JointPtr fr_axle_joint;
         private: common::PID fl_pid, fr_pid;
         private: std::unique_ptr<ros::NodeHandle> ros_node;
         private: ros::Subscriber rosSub, fl_sub, fr_sub;
+        private: ros::Publisher odo_fl_pub, odo_fr_pub;
 
-        /// \brief A ROS callbackqueue that helps process messages
         private: ros::CallbackQueue ros_queue;
 
-        /// \brief A thread the keeps running the ros_queue
-        private: std::thread ros_queueThread;
+        private: std::thread ros_queue_thread;
+
+        private: void publish_state(){
+            const int ticks_per_revolution = 42;
+
+            std_msgs::Int32 odo_fl;
+            odo_fl.data = (int) fl_axle_joint->Position() * ticks_per_revolution / (M_2_PI);
+            odo_fl_pub.publish(odo_fl);
+
+            std_msgs::Int32 odo_fr;
+            odo_fr.data = (int) fr_axle_joint->Position() * ticks_per_revolution / (M_2_PI);
+            odo_fr_pub.publish(odo_fr);
+        }
 
 
         public: void on_fl_str(const std_msgs::Float32ConstPtr &_msg)
@@ -35,17 +49,20 @@ namespace gazebo  {
                 fr_str_joint->GetScopedName(), _msg->data);
         }
 
-        /// \brief ROS helper function that processes messages
         private: void QueueThread()
         {
-        static const double timeout = 0.01;
-        while (ros_node->ok())
-        {
-            ros_queue.callAvailable(ros::WallDuration(timeout));
+            static const double timeout = 0.001;
+            ros::Rate loop_rate(100);
+
+            while (ros_node->ok())
+            {
+                ros_queue.callAvailable(ros::WallDuration(timeout));
+                publish_state();
+                loop_rate.sleep();
+            }
         }
-        }
-        public:
-        FakeCarPlugin() : ModelPlugin()
+        
+        public: FakeCarPlugin() : ModelPlugin()
         {
             ROS_INFO("FakeCarPlugin() : ModelPlugin()");
         }
@@ -53,12 +70,11 @@ namespace gazebo  {
 
         void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
         {
-            // Make sure the ROS node for Gazebo has already been initialized                                                                                    
             if (!ros::isInitialized())
             {
-            ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
-                << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
-            return;
+                ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
+                    << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+                return;
             }
             ROS_INFO("hello fake_car!");
 
@@ -80,22 +96,25 @@ namespace gazebo  {
             fr_str_joint  = _model->GetJoint("front_right_wheel_steer_joint");
             ROS_INFO("Found fr_str_joint %s", fr_str_joint->GetScopedName().c_str());
 
+            fl_axle_joint = _model->GetJoint("front_left_wheel_joint");
+            fr_axle_joint = _model->GetJoint("front_right_wheel_joint");
+
             model->GetJointController()->SetPositionPID(
                 fr_str_joint->GetScopedName(), fr_pid);
 
-            // Initialize ros, if it has not already bee initialized.
             if (!ros::isInitialized())
             {
-            int argc = 0;
-            char **argv = NULL;
-            ros::init(argc, argv, "fake_car_plugin",
-                ros::init_options::NoSigintHandler);
+                int argc = 0;
+                char **argv = NULL;
+                ros::init(argc, argv, "fake_car_plugin",
+                    ros::init_options::NoSigintHandler);
             }
 
-            // Create our ROS node. This acts in a similar manner to
-            // the Gazebo node
             
             ros_node.reset(new ros::NodeHandle("fake_car_plugin"));
+
+            odo_fl_pub = ros_node->advertise<std_msgs::Int32>("/" + model->GetName() + "/odo_fl", 10);
+            odo_fr_pub = ros_node->advertise<std_msgs::Int32>("/" + model->GetName() + "/odo_fr", 10);
 
             fl_sub = ros_node->subscribe<std_msgs::Float32>(
                 "/" + model->GetName() + "/fl_str",
@@ -106,20 +125,8 @@ namespace gazebo  {
                 "/" + model->GetName() + "/fr_str",
                 2,
                 &FakeCarPlugin::on_fr_str, this);
-/*
-            rosSub = ros_node->subscribe(ros::SubscribeOptions::create<std_msgs::Float32>(
-                "/" + model->GetName() + "/fl_str",
-                1,
-                boost::bind(&FakeCarPlugin::on_fl_str, this, _1),
-                ros::VoidPtr(), &ros_queue));
 
-            rosSub = ros_node->subscribe(ros::SubscribeOptions::create<std_msgs::Float32>(
-                "/" + model->GetName() + "/fr_str",
-                1,
-                boost::bind(&FakeCarPlugin::on_fr_str, this, _1),
-                ros::VoidPtr(), &ros_queue));
-*/
-            ros_queueThread = std::thread(std::bind(&FakeCarPlugin::QueueThread, this));
+            ros_queue_thread = std::thread(std::bind(&FakeCarPlugin::QueueThread, this));
         }
     };
 
